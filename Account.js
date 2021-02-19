55f2d717b31ab14d884938a2d41ce92a5609560a
CoderIllusionist <75251048+CoderIllusionist@users.noreply.github.com>
2021-02-19 14:02:28 +0100

    Account.class & BusinessAccount.class

--- Account.js
+++ Account.js
@@ -1,2 +1,19 @@
-// Export your class here as module.exports = MyClass
-module.exports = null;
+class Account {
+    constructor(name, balance, creditLimit) {
+        this.name = name;
+        this.balance = balance;
+        this.creditLimit = creditLimit;
+    }
+    makeDeposit(amount) {
+        if (amount < 0) {return};
+        this.balance = this.balance + amount;
+        return this.balance;
+    }
+    makeWithdrawal(amount) {
+        if(amount > this.balance && amount > this.creditLimit) {return};
+        if(amount > 5000) {return};
+        this.balance = this.balance - amount;
+    }
+}
+
+module.exports = Account;
--- BusinessAccount.js
+++ BusinessAccount.js
@@ -1,2 +1,30 @@
+class BusinessAccount {
+    constructor(name, balance, creditLimit, sepaPermission) {
+        this.name = name;
+        this.balance = balance;
+        this.creditLimit = creditLimit;
+        this.sepaPermission = sepaPermission;
+    }
+    makeDeposit(amount) {
+        if (amount < 0) {return};
+        this.balance = this.balance + amount;
+        return this.balance;
+    }
+    makeWithdrawal(amount) {
+        if(amount > this.balance && amount > this.creditLimit) {return};
+        if(amount > 20000) {return};
+        this.balance = this.balance - amount;
+    }
+    sepaInvoice(amount) {
+        if (!this.sepaPermission) {return};
+        if (amount < 0) {return};
+        const minusOnePercent =  amount - (amount * .01)
+        this.balance = this.balance + minusOnePercent;
+        return this.balance;
+    }
+}
+
 // Export your class here as module.exports = MyClass
-module.exports = null;
+module.exports = BusinessAccount;
+
+
