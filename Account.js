c9978afb2b76ea40d5d69c3b86b2753f91ed772d
CoderIllusionist <75251048+CoderIllusionist@users.noreply.github.com>
2021-02-19 14:13:57 +0100

    BusinessAccount Extends Account

--- Account.js
+++ Account.js
@@ -16,4 +16,25 @@ class Account {
     }
 }
 
+class BusinessAccount extends Account() {
+    constructor(name, balance, creditLimit) {
+        super();
+    }
+    makeWithdrawal(amount) {
+        if(amount > this.balance && amount > this.creditLimit) {return};
+        if(amount > 20000) {return};
+        this.balance = this.balance - amount;
+    }
+    sepaInvoice(amount) {
+        if (!this.sepaPermission) {return};
+        if (amount < 0) {return};
+        const minusOnePercent =  amount - (amount * .01)
+        this.balance = this.balance + minusOnePercent;
+        return this.balance;
+    }
+}
+const businessAccount = new BusinessAccount('icasa', 500000, 1000000);
+console.log(businessAccount.this.balance)
+businessAccount.makeWithdrawal(20001)
+console.log(businessAccount.this.balance)
 module.exports = Account;
